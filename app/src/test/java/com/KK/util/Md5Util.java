1c8759ca3cf85b7f369a801d73140039a718df8e
keyupan <949609036@qq.com>
2020-08-29 17:39:26 +0800

    test

---
+++ app/src/test/java/com/KK/util/Md5Util.java
@@ -0,0 +1,21 @@
+package com.KK.util;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Md5Util {
+    public static String getMD5String(String source){
+        try {
+            MessageDigest md=MessageDigest.getInstance("MD5");
+            byte[] bytes=md.digest(source.getBytes());
+            StringBuilder sb=new StringBuilder();
+            for(byte b :bytes){
+                sb.append(String.format("%02X", b));
+            }
+            return sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+}
